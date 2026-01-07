76c446f90d24b336a7444c71e46cbd81b7a1e24e
vladt <vladtihonov1183@gmail.com>
2026-01-07 14:00:25 +0100

    e

--- About.py
+++ About.py
@@ -5,15 +5,25 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "My Arcade Game"
 
+class S1mple(arcade.Sprite):
+    def __init__(self):
+        super().__init__("img_1.png",0.45)
+        self.center_x = 380
+        self.center_y = 300
 
+class Potujno(arcade.Sprite):
+    def __init__(self):
+        super().__init__("img.png", 0.45)
+        self.center_x = 380
+        self.center_y = 300
 class Game(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         arcade.set_background_color(arcade.color.BLACK)
+        self.Bplant = S1mple()
+        self.Vorobushek = Potujno()
 
-        # Здесь будут переменные игры
-        # пример:
-        # self.player = None
+
 
     def setup(self):
         """Вызывается один раз при старте или перезапуске"""
@@ -22,8 +32,8 @@ class Game(arcade.Window):
     def on_draw(self):
         """Отрисовка"""
         arcade.start_render()
-        # Рисовать здесь
-        pass
+        self.Bplant.draw()
+        self.Vorobushek.draw()
 
     def on_update(self, delta_time):
         """Логика игры (60 раз в секунду)"""
